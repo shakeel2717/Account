35a8598180dd3438b66f139a87009bc1e9b8c9fc
Shakeel Ahmad <shakeel2717@gmail.com>
2022-12-09 23:46:19 -0800

    Email Required on Signup

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,39 +1,43 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+@extends('layout.auth')
+@section('form')
+<div class="card card-bordered">
+    <div class="card-inner card-inner-lg">
+        <div class="nk-block-head">
+            <div class="nk-block-head-content">
+                <h4 class="nk-block-title">Account Created Successfully</h4>
+                <div class="nk-block-des">
+                    <hr>
+                    <p>Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.</p>
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-        </div>
-
-        @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided during registration.') }}
-            </div>
-        @endif
-
-        <div class="mt-4 flex items-center justify-between">
-            <form method="POST" action="{{ route('verification.send') }}">
-                @csrf
-
-                <div>
-                    <x-primary-button>
-                        {{ __('Resend Verification Email') }}
-                    </x-primary-button>
+                    @if (session('status') == 'verification-link-sent')
+                    <div class="m-2 text-success">
+                        {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+                    </div>
+                    @endif
                 </div>
-            </form>
-
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-
-                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    {{ __('Log Out') }}
-                </button>
-            </form>
+            </div>
         </div>
-    </x-auth-card>
-</x-guest-layout>
+        <div class="row">
+            <div class="col-md-6">
+                <form action="{{ route('verification.send') }}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <button class="btn btn-lg btn-primary btn-block">Resend Email</button>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-6">
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <div class="form-group">
+                        <button class="btn btn-lg btn-primary btn-block">Sign out</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="form-note-s2 text-center pt-4"> Don't have an account? <a href="{{ route('register') }}"><strong>Create new Account!</strong></a>
+        </div>
+    </div>
+</div>
+@endsection
