111aefb9a73ac80e3f8e06be28c592c2dde40ba3
Shakeel Ahmad <shakeel2717@gmail.com>
2022-12-09 22:52:21 -0800

    first commit

---
+++ resources/views/layout/auth.blade.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="zxx" class="js">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="ASAN Webs Development">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="{{ env('APP_DESC') }}">
+    <meta name="keywords" content="{{ env('APP_KEYWORDS') }}">
+    <link rel="shortcut icon" href="{{ asset('assets/svg/favi.svg') }}">
+    <title>{{ env('APP_NAME') }} | {{ env('APP_DESC') }}</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/dashlite.css?ver=3.1.0') }}">
+    <link id="skin-default" rel="stylesheet" href="{{ asset('assets/css/theme.css?ver=3.1.0') }}">
+</head>
+
+<body class="nk-body bg-white npc-default pg-auth">
+    <div class="nk-app-root">
+        <div class="nk-main ">
+            <div class="nk-wrap nk-wrap-nosidebar">
+                <div class="nk-content ">
+                    <div class="nk-block nk-block-middle nk-auth-body wide-xs">
+                        <div class="brand-logo pb-4 text-center">
+                            <a href="" class="logo-link">
+                                <img class="logo-light logo-img logo-img-lg" src="{{ asset('assets/svg/logo-light.svg') }}" srcset="{{ asset('assets/svg/logo-light.svg') }} 2x" alt="logo">
+                                <img class="logo-dark logo-img logo-img-lg" src="{{ asset('assets/svg/logo.svg') }}" srcset="{{ asset('assets/svg/logo.svg') }} 2x" alt="logo-dark">
+                            </a>
+                        </div>
+                        <div class="card card-bordered">
+                            <div class="card-inner card-inner-lg">
+                                <div class="nk-block-head">
+                                    <div class="nk-block-head-content">
+                                        <h4 class="nk-block-title">Register</h4>
+                                        <div class="nk-block-des">
+                                            <p>Create New {{ env('APP_NAME') }} Account</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <form action="html/pages/auths/auth-success-v2.html">
+                                    <div class="form-group">
+                                        <label class="form-label" for="name">Name</label>
+                                        <div class="form-control-wrap">
+                                            <input type="text" class="form-control form-control-lg" id="name" placeholder="Enter your name">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="email">Email or Username</label>
+                                        <div class="form-control-wrap">
+                                            <input type="text" class="form-control form-control-lg" id="email" placeholder="Enter your email address or username">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="form-label" for="password">Passcode</label>
+                                        <div class="form-control-wrap">
+                                            <a href="#" class="form-icon form-icon-right passcode-switch lg" data-target="password">
+                                                <em class="passcode-icon icon-show icon ni ni-eye"></em>
+                                                <em class="passcode-icon icon-hide icon ni ni-eye-off"></em>
+                                            </a>
+                                            <input type="password" class="form-control form-control-lg" id="password" placeholder="Enter your passcode">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-control-xs custom-checkbox">
+                                            <input type="checkbox" class="custom-control-input" id="checkbox">
+                                            <label class="custom-control-label" for="checkbox">I agree to {{ env('APP_NAME') }} <a href="#">Privacy Policy</a> &amp; <a href="#"> Terms.</a></label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <button class="btn btn-lg btn-primary btn-block">Register</button>
+                                    </div>
+                                </form>
+                                <div class="form-note-s2 text-center pt-4"> Already have an account? <a href="html/pages/auths/auth-login-v2.html"><strong>Sign in instead</strong></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="nk-footer nk-auth-footer-full">
+                        <div class="container wide-lg">
+                            <div class="row g-3">
+                                <div class="col-lg-6 order-lg-last">
+                                    <ul class="nav nav-sm justify-content-center justify-content-lg-end">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Terms & Condition</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Privacy Policy</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-lg-6">
+                                    <div class="nk-block-content text-center text-lg-left">
+                                        <p class="text-soft">&copy; 2022 {{ env('APP_NAME') }}. All Rights Reserved.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="{{ asset('assets/js/bundle.js?ver=3.1.0') }}"></script>
+    <script src="{{ asset('assets/js/scripts.js?ver=3.1.0') }}"></script>
+
+</html>
