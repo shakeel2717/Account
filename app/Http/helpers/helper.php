e0bfad8a10e2714e989a250166033042f6724860
Shakeel Ahmad <shakeel2717@gmail.com>
2022-12-12 01:46:18 -0800

    User Dashboard Stats Complete

--- app/Http/helpers/helper.php
+++ app/Http/helpers/helper.php
@@ -23,11 +23,36 @@ function withdraw($user_id)
     return $in;
 }
 
+function firstLevelBonus($user_id)
+{
+    $in = Transaction::where('user_id', $user_id)
+        ->where('type', 'Level 1 Commission')
+        ->where('sum', true)
+        ->sum('amount');
+    return $in;
+}
+function secondLevelBonus($user_id)
+{
+    $in = Transaction::where('user_id', $user_id)
+        ->where('type', 'Level 2 Commission')
+        ->where('sum', true)
+        ->sum('amount');
+    return $in;
+}
+
+function thirdLevelBonus($user_id)
+{
+    $in = Transaction::where('user_id', $user_id)
+        ->where('type', 'Level 3 Commission')
+        ->where('sum', true)
+        ->sum('amount');
+    return $in;
+}
+
 
 function bonus($user_id)
 {
-    $in = Transaction::where('user_id', $user_id)->where('type', 'Profit')->where('sum', true)->sum('amount');
-    return $in;
+    return firstLevelBonus($user_id) + secondLevelBonus($user_id) + thirdLevelBonus($user_id);
 }
 
 
