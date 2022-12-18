3af78b691a06cc1bedf0fc969d5a66d5badcf77b
Shakeel Ahmad <shakeel2717@gmail.com>
2022-12-18 05:38:02 -0800

    total out removed

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -27,15 +27,6 @@
         <div class="col-md-4">
             <div class="card card-bordered mb-2">
                 <div class="card-body">
-                    <h4 class="title">Total Out</h4>
-                    <br>
-                    <h2>{{ number_format(totalOut(),2) }}</h2>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card card-bordered mb-2">
-                <div class="card-body">
                     <h4 class="title">Total In</h4>
                     <br>
                     <h2>{{ number_format(totalIn(),2) }}</h2>
